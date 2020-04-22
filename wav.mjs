d7a4cd9cd21bfdbecfa9f3e2d90087114a87b78a
Sebastian Simon <37915283+SebastianSimon@users.noreply.github.com>
2020-04-22 15:50:21 +0200

    Improve old WAV code

--- wav.mjs
+++ wav.mjs
@@ -1,15 +1,19 @@
 /* eslint no-unused-vars: [ "error", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" } ] */
 /* eslint no-trailing-spaces: [ "error", { "skipBlankLines": true } ] */
 
-export default function wav(randCurve){
-  const littleEndian = new Uint8ClampedArray(new Uint16Array([1]).buffer)[0] === 1,
+export default function wav(waveData){
+  const littleEndian = new Uint8ClampedArray(new Uint16Array([
+      1
+    ]).buffer)[0] === 1,
     arrayTypes = {
       2: Uint16Array,
       4: Uint32Array
     },
     typedNumber = (dataLength, bytes = 4) => {
       const TypedArray = arrayTypes[bytes] ?? Uint8ClampedArray,
-        array = new Uint8ClampedArray(new TypedArray([dataLength]).buffer);
+        array = new Uint8ClampedArray(new TypedArray([
+          dataLength
+        ]).buffer);
     
       return littleEndian
         ? array
@@ -19,13 +23,16 @@ export default function wav(randCurve){
     wavProps = {
       channels: 1,
       bytesPerSample: 2,
-      sampleRate: 44100
+      sampleRate: 44100,
+      formats: {
+        PCM: 1
+      }
     },
     wavSettings = {
       riff: Array.from("RIFF", toChar),
       wave: Array.from("WAVEfmt ", toChar),
       fmtLength: typedNumber(16, 4),
-      formatTag: typedNumber(1, 2), // PCM
+      formatTag: typedNumber(wavProps.formats.PCM, 2),
       channels: typedNumber(wavProps.channels, 2),
       sampleRate: typedNumber(wavProps.sampleRate, 4),
       bytesPerSec: typedNumber(wavProps.sampleRate * wavProps.bytesPerSample, 4),
@@ -37,7 +44,7 @@ export default function wav(randCurve){
     lengthPlaceholder = new Array(4).fill(0),
     header = [
       ...wavSettings.riff,
-      ...lengthPlaceholder.slice(),
+      ...lengthPlaceholder,
       ...[
         "wave",
         "fmtLength",
@@ -49,14 +56,14 @@ export default function wav(randCurve){
         "bitsPerSample",
         "data"
       ].flatMap(wavProp),
-      ...lengthPlaceholder.slice()
+      ...lengthPlaceholder
     ],
-    wavFile = new Uint8ClampedArray(wavProps.bytesPerSample * wavProps.channels * randCurve.length + header.length);
+    wavFile = new Uint8ClampedArray(wavProps.bytesPerSample * wavProps.channels * waveData.length + header.length);
   
   wavFile.set(header);
   wavFile.set(typedNumber(wavFile.length - wavSettings.riff.length - lengthPlaceholder.length, lengthPlaceholder.length), wavSettings.riff.length);
   wavFile.set(typedNumber(wavFile.length - header.length, lengthPlaceholder.length), header.length - lengthPlaceholder.length);
-  wavFile.set(new Uint8ClampedArray(new Int16Array(randCurve.map((sample) => Math.floor(sample * (2 ** (wavProps.bytesPerSample * 8 - 1))))).buffer), header.length);
+  wavFile.set(new Uint8ClampedArray(new Int16Array(waveData.map((sample) => Math.floor(sample * (2 ** (wavProps.bytesPerSample * 8 - 1))))).buffer), header.length);
   
   return URL.createObjectURL(new Blob([
       wavFile
